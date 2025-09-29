9304bf8d52127d7d087e4fb9fa435401cf30bb57
Mohammed Hujairi <75939400+nightymighty68@users.noreply.github.com>
2025-09-29 09:55:16 +0300

    anova table in r

--- weee.R
+++ weee.R
@@ -1,1 +1,9 @@
-print("hellow world")
+# Using car package
+install.packages("car")
+library(car)
+Anova(model, type = "II")  # Type II ANOVA
+
+# Using broom package
+install.packages("broom")
+library(broom)
+tidy(anova(model))
