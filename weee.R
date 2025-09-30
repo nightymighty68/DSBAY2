a9bfc5c565e984b06389f77c13031dbce67df878
Mohammed Hujairi <75939400+nightymighty68@users.noreply.github.com>
2025-09-30 08:32:22 +0300

    r tests matrix

--- weee.R
+++ weee.R
@@ -1,9 +1,14 @@
-# Using car package
-install.packages("car")
-library(car)
-Anova(model, type = "II")  # Type II ANOVA
+earnings_ali <- c(50,60,55,74,80)
+earnings_jameela <-c(53,57,79,88,93)
 
-# Using broom package
-install.packages("broom")
-library(broom)
-tidy(anova(model))
+weekdays<- c("Sunday","Monday","Tuesday","Wednesday","Thursday")
+
+merchant_names<- c("ali","jameela")
+
+earnings_matrix <- matrix(c(earnings_ali,earnings_jameela), byrow = TRUE, nrow = 2)
+
+rownames <- merchant_names
+
+colnames<- weekdays
+
+earnings_matrix
